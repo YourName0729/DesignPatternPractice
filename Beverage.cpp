0aca7ef96acd4824e2a65e7abfb6740ffe975062
YourName0729 <40850028+YourName0729@users.noreply.github.com>
2018-07-23 14:33:38 +0800

    UD Beverage

--- Beverage.cpp
+++ Beverage.cpp
@@ -6,102 +6,90 @@
 using namespace std;
 
 class Beverage {
-private:	
-	string brew;
-	string flavor;
-	string mix;
-public:
-	class Builder;
-	Beverage(){}
-	Beverage(string b, string f, string m) : brew(b), flavor(f), mix(m){}
-	void print() {
-		printf("brew with %s\n", brew.c_str());
-		printf("add %s for flavor\n", flavor.c_str());
-		printf("mix with %s\n\n", mix.c_str());
-	}
+	
+}; 
+class MilkTea : public Beverage {
+	
+};
+class Coffee : public Beverage {
+	
 };
 
-class Beverage::Builder {
-private:
-	string brew;
-	string flavor;
-	string mix;
+class Builder {
 public:
-	Builder(){}
-	Builder(string b, string f, string m) : brew(b), flavor(f), mix(m){}
-	Builder& setBrew(string b){
-		brew = b;
-		return *this;
-	}
-	Builder& setFlavor(string f){
-		flavor = f;
-		return *this;
-	}
-	Builder& setMix(string m){
-		mix = m;
-		return *this;
-	}
-	Builder& setMilkTea(){
-		brew = "Black Tea";
-		flavor = "Milk";
-		mix = "Chocolate Powder";
-		return *this;
-	}
-	Builder& setCoffee(){
-		brew = "Coffee Beans";
-		flavor = "Creamer";
-		mix = "Cinnamon Powder";
-		return *this;
-	}
-	Beverage build() {
-		return Beverage(brew, flavor, mix);
-	}
+	virtual Beverage& build() = 0;
+	virtual void brew() = 0;
+	virtual void flavor() = 0;
+	virtual void mix() = 0;
 };
-/*class MilkTea : public Beverage {
+class MilkTeaBuilder : public Builder {
 public:
-	void brew(){
-		printf("brew black tea\n");
+	void brew() {
+		printf("brew with black tea\n");
 	}
-	void flavor(){
+	void flavor() {
 		printf("add milk\n");
 	}
 	void mix() {
-		printf("add chocolate\n");
+		printf("mix with chocolate\n");
+	}
+	Beverage& build() {
+		MilkTea mt;
+		brew();
+		flavor();
+		mix();
+		return mt;
 	}
 };
-
-class Coffee : public Beverage {
+class CoffeeBuilder : public Builder {
 public:
-	void brew(){
-		printf("brew coffee beans\n");
+	void brew() {
+		printf("milling\n");
 	}
-	void flavor(){
+	void flavor() {
 		printf("add creamer\n");
 	}
 	void mix() {
-		printf("add Cinnamon powder\n");
+		printf("mix with cinnamon powder\n");
 	}
-};*/
+	Beverage& build() {
+		Coffee cf;
+		brew();
+		flavor();
+		mix();
+		return cf;
+	}
+};
+class BuilderDirector {
+private:
+	Builder* builder;
+public:
+	enum BuilderType {
+		milkTeaB,
+		coffeeB
+	};
+	BuilderDirector(BuilderType bt) {
+		switch(bt) {
+			case milkTeaB:
+				builder = new MilkTeaBuilder();
+				break;
+			case coffeeB:
+				builder = new CoffeeBuilder();
+				break;
+		}
+	}
+	Beverage& build() {
+		return builder->build();
+	}
+};
 
 int main() {
-//Way1
-	//MilkTea
-	Beverage milkTea = Beverage::Builder().setMilkTea().build();
-	milkTea.print();
-	//Coffee
-	Beverage coffee = Beverage::Builder().setCoffee().build();
-	coffee.print();
-//Way2
-	Beverage milkTea2 = Beverage::Builder()
-						.setBrew("Black Tea")
-						.setFlavor("Milk")
-						.setMix("Chocolate Powder")
-						.build();
-	milkTea2.print();
-//Way3
-	Beverage::Builder b;
-	b.setCoffee();
-	Beverage coffee2 = b.build();
-	coffee2.print();
+	BuilderDirector builderDirector1(BuilderDirector::milkTeaB);
+	Beverage milkTea = builderDirector1.build();
+	printf("\n");
+	
+	BuilderDirector builderDirector2(BuilderDirector::coffeeB);
+	Beverage coffee = builderDirector2.build();
+	
 	return 0;
 }
